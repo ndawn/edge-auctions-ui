747b720decb4c3f723cde15b62522299dfa3ab5f
Max <burmistrovm@live.ru>
2023-04-22 23:39:31 +0200

    fixes

--- src/components/auctions/AuctionsGrid.jsx
+++ src/components/auctions/AuctionsGrid.jsx
@@ -5,7 +5,7 @@ import AuctionCard from 'components/auctions/AuctionCard';
 const AuctionsGrid = ({ auctions, noMargin = false }) => (
   <Grid container spacing={4} sx={{ marginTop: noMargin ? '0' : '48px', paddingBottom: '48px' }}>
     {auctions.map((auction) => (
-      <Grid key={auction.id} item xs={6} md={4} lg={3}>
+      <Grid key={auction.id} item xs={12} sm={6} md={4} lg={3}>
         <AuctionCard auction={auction} />
       </Grid>
     ))}
